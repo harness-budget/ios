4aa17313784ea71c80d7fbf8124416c7400a727f
Ben Aubin <ben@benaubin.com>
2021-02-27 13:31:39 -0600

    Initial Commit

---
+++ Harness/HarnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarnessApp.swift
+//  Harness
+//
+//  Created by Ben Aubin on 2/27/21.
+//
+
+import SwiftUI
+
+@main
+struct HarnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
