6cfd56f31112f128150c709004cf1603330e55dc
Andrew Carter <andrew@mintere.com>
2021-02-28 08:33:47 -0600

    Add Goal UI

--- Shared/AddGoalView.swift
+++ Shared/AddGoalView.swift
@@ -6,3 +6,56 @@
 //
 
 import Foundation
+import SwiftUI
+
+
+struct AddGoalView: View {
+    
+    
+    @Binding var safeToSpend: Int
+    @State private var name: String = ""
+    @State private var amount: Int = 0
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading) {
+                
+                Text("What are you setting aside for?")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                TextField("Name", text: $name)
+                
+                
+                Spacer()
+                HStack{
+                    Spacer()
+                    MoneyInputView(value: $amount)
+                    Spacer()
+                }
+                Spacer()
+                Button (action: {}, label: {
+                    HStack {
+                        Spacer()
+                        Text("Set Aside")
+                            .fontWeight(.semibold)
+                            .padding()
+                        Spacer()
+                    }
+                    .cornerRadius(8.0)
+                })
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(12.0)
+                .padding()
+            }
+            .padding(.horizontal, 16)
+            .navigationTitle("Set Aside")
+        }
+    }
+}
+
+struct AddGoalView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddGoalView(safeToSpend: Binding.constant(75000))
+    }
+}
--- Shared/MoneyInputView.swift
+++ Shared/MoneyInputView.swift
@@ -6,3 +6,83 @@
 //
 
 import Foundation
+import SwiftUI
+
+
+struct MoneyInputView: View {
+    
+    @Binding var value: Int
+    var gridItemLayout = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+    
+    func addNumber(number: Int) {
+        value *= 10
+        value += number
+    }
+    
+    func dropNumber() {
+        value /= 10
+    }
+    
+    func clear() {
+        value = 0
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("$\(value / 100).\(String(format: "%02d", value % 100))")
+                    .font(.title)
+                    .fontWeight(.bold)
+            }
+            LazyVGrid(columns: gridItemLayout) {
+                ForEach((1...9), id: \.self) { i in
+                    Button (action: {addNumber(number: i)}, label: {
+                        HStack {
+                            Spacer()
+                            Text("\(i)")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                            Spacer()
+                        }
+                    }).padding()
+                }
+            }
+            HStack() {
+                
+                Button (action: {clear()}, label: {
+                    HStack {
+                        Spacer()
+                        Image(systemName: "clear")
+                        Spacer()
+                    }
+                }).padding()
+                
+                Button (action: {addNumber(number: 0)}, label: {
+                    HStack {
+                        Spacer()
+                        Text("0")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                        Spacer()
+                    }
+                }).padding()
+                
+                Button (action: {dropNumber()}, label: {
+                    HStack {
+                        Spacer()
+                        Image(systemName: "delete.left")
+                        Spacer()
+                    }
+                }).padding()
+                
+                
+            }
+        }.frame(width: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MoneyInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        MoneyInputView(value: Binding.constant(50025))
+    }
+}
